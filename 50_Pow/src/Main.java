075a65a705ac1444ec4529e7dd6c316afe497ca1
damdinhmanh <manhdam.198@gmail.com>
2022-10-09 23:22:47 +0700

    submit
    submit

---
+++ 50_Pow/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        double ret = solution.myPow(1.2, 2187);
+        System.out.println(ret);
+    }
+}
